d9358d9e70ed2901d75044b35ee6a8dd0b50e537
Garv <garv.programming@gmail.com>
2021-03-12 14:01:20 +0530

    Added comments and readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# Thesaurus
+Wanna stylize your writings? Thesaurus at your service!
+
+Any file you are working with, thesaurus' scraper finds you the synonyms for the last word you typed!
+
+## Initialization
+1. ``` pip -r requirements.txt ```
+2. ``` python main.py ```
+
+Now start working on your essay/whatever and thesaurus will do its job!
+
+### For linux users
+This app uses keyboard module, so you have to run it as root.
+Just make sure you run the above commands with ```sudo``` prefix.
+
+## Credits
+The data is being provided from [thesaurus.yourdictionary](https://thesaurus.yourdictionary.com/)
--- main.py
+++ main.py
@@ -1,24 +1,40 @@
+# Importing required modules
 from bs4 import BeautifulSoup 
 import requests 
 import keyboard
-import os
+
 
 def get_current_word():
+    ''' 
+    Recording the key presses until <space> is pressed,
+    i.e. gets the current word typed.
+    '''
+
     return keyboard.get_typed_strings(keyboard.record(until='space'))
 
+
+# A few phrases that need not be considered for synonym check.
+auxillary_phrases = ['is', 'am', 'are', 'has', 'have', 'had', 'been', 'was',
+'would', 'should', 'shall', 'may', 'might', 'a', 'an', 'the', 'you', 'we',
+'i','of']
+
+
 while True:
-    word = list(get_current_word())[0].strip()
+    word = list(get_current_word())[0].strip().lower()
 
-    if word.isalpha():
+    # Searching for the word's meaning.
+    if word.isalpha() and word not in auxillary_phrases:
 
-        search_url = "https://thesaurus.yourdictionary.com/" + word.lower()
+        # Scraping the data from the website.
+        search_url = "https://thesaurus.yourdictionary.com/" + word
         response = requests.get(search_url)
         soup = BeautifulSoup(response.content , features = 'html.parser')
 
+        # Getting the first '5' synonyms
         synonyms = soup.find_all('a', {'class': 'synonym-link'})[:5]
 
         if len(synonyms) != 0:
-            print("Synonyms for", word, "==>")
+            print(f"Synonyms for '{word}' ==>")
             [print("-", synonym.text) for synonym in synonyms]
             print()
 
