c0b77621509c019735b0729a6c6e062dda80a7e3
Garv <garv.programming@gmail.com>
2021-03-12 13:33:04 +0530

    Key-record feature

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from bs4 import BeautifulSoup 
 import requests 
+import keyboard
+import os
 
-word = input("Enter a word: ").strip()
+def get_current_word():
+    return keyboard.get_typed_strings(keyboard.record(until='space'))
 
-search_url = "https://thesaurus.yourdictionary.com/" + word.lower()
-response = requests.get(search_url)
-soup = BeautifulSoup(response.content , features = 'html.parser')
+while True:
+    word = list(get_current_word())[0].strip()
 
-l = soup.find_all('a', {'class': 'synonym-link'})[:8]
+    if word.isalpha():
 
-[print(synonym.text) for synonym in l]
+        search_url = "https://thesaurus.yourdictionary.com/" + word.lower()
+        response = requests.get(search_url)
+        soup = BeautifulSoup(response.content , features = 'html.parser')
+
+        synonyms = soup.find_all('a', {'class': 'synonym-link'})[:5]
+
+        if len(synonyms) != 0:
+            print("Synonyms for", word, "==>")
+            [print("-", synonym.text) for synonym in synonyms]
+            print()
+
+        else:
+            print(f"Sorry, nothing matched for '{word}'")
+            print()
